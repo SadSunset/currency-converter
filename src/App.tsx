a96edc6fc884aa1e0293e9c10cc5f38d6dc0c5c8
Alexander Semyahin <alexsander200121@gmail.com>
2023-11-30 21:58:26 +0300

    create project and fetch data

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+
+
+interface ICurrencyInformation {
+  ID: string;
+  Name: string;
+  Nominal: number;
+  CharCode: string;
+  Value: number;
+}
+
 
 function App() {
+
+  const [data, setData] = useState<ICurrencyInformation[]>([])
+
+  useEffect(() => {
+    fetch('https://www.cbr-xml-daily.ru/daily_json.js')
+      .then(response => response.json())
+      .then(data => data.Valute)
+      .then(valuteInformation => {
+        const listOfValute = Object.values<ICurrencyInformation>(valuteInformation)
+        setData([...listOfValute])
+      })
+      .catch(err => console.error(err.message))
+  }, [])
+
+  console.log('data', data)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>+100</div>
+      <table>
+        <thead>
+          <tr>
+            <th>Валюта</th>
+            <th>Единиц</th>
+            <th>Буквенный код</th>
+            <th>Курс</th>
+          </tr>
+        </thead>
+        <tbody>
+          {Array.isArray(data) && data.map((item: any) =>
+            <tr key={item.ID}>
+              <td>{item.Name}</td>
+              <td>{item.Nominal}</td>
+              <td>{item.CharCode}</td>
+              <td>{item.Value}</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
     </div>
   );
 }
